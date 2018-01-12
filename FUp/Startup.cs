5396dc823ee253b0f3eeb622e5aaee8def85a2f9
yassine <yakinedev@gmail.com>
2018-01-12 19:37:30 +0000

    Add project files.

---
+++ FUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FUp.Startup))]
+namespace FUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
