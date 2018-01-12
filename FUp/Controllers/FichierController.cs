a3d999fcbff8cfd8d504f6c4d66209bc35c0748a
yassine <yakinedev@gmail.com>
2018-01-12 20:19:57 +0000

    Video 2

---
+++ FUp/Controllers/FichierController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FUp.Models;
+
+namespace FUp.Controllers
+{
+    public class FichierController : Controller
+    {
+        // GET: Fichier
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show(int id)
+        {
+            Fichier f = new Fichier();
+            f.Id = 1;
+            f.Nom = "fichier.txt";
+            f.Taile = 0.1f;
+
+            return View(f);
+        }
+    }
+}
---
+++ FUp/Models/Fichier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FUp.Models
+{
+    public class Fichier
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public float Taile { get; set; }
+    }
+}
